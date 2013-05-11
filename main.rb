7e5fe7babb58f3bf3c9b5a93f9944b47560724dc
Brandon Richey <richeyb@gmail.com>
2013-05-10 21:43:46 -0400

    Adding the first enemy!

--- main.rb
+++ main.rb
@@ -23,6 +23,7 @@ class Main
     @background.fill([0, 0, 0])
 
     @player = Player.new
+    @enemy  = Enemy.new
     @food   = []
     Constants::PELLETS.times { @food << Food.new }
   end
@@ -38,12 +39,14 @@ class Main
   def draw
     @screen.fill [0, 0, 0]
     @player.draw(@screen)
+    @enemy.draw(@screen)
     @food.each { |pellet| pellet.draw(@screen) }
     @screen.flip
   end
 
   def update
     @player.update
+    @enemy.update
 
     @food.each do |pellet|
       if pellet.full_collide? @player
@@ -200,6 +203,18 @@ class GameObject
     @image.blit(surface, [x, y])
   end
 
+  def move
+    if up?
+      move_up
+    elsif down?
+      move_down
+    elsif left?
+      move_left
+    else
+      move_right
+    end
+  end
+
   def move_left
     @direction = :left
     @vel.x = -Constants::PLAYER_SPEED
@@ -289,8 +304,8 @@ class Food < GameObject
 end
 
 class Player < GameObject
-  def initialize
-    super
+  def initialize(color=[255, 255, 255])
+    super(color)
     @pos.x = 312
     @pos.y = 600
     @direction = :up
@@ -359,6 +374,78 @@ class Player < GameObject
       piece.y -= offset
     end
     return piece
+  end
+end
+
+class Enemy < Player
+  def initialize(color=[0,0,255])
+    super(color)
+    @pos.x = rand(20..620)
+    @pos.y = rand(20..620)
+
+    @pieces = []
+    @turns  = []
+
+    change_direction
+    5.times { add_piece }
+    reset_time
+
+    move
+  end
+
+  def reset_time
+    @time_until_turn = rand(3..8) * 4
+  end
+
+  def update
+    if @time_until_turn <= 0
+      reset_time
+      change_direction
+    else
+      @time_until_turn -= 1
+    end
+    super
+  end
+
+  def change_direction
+    directions = []
+    if (up? || down?)
+      directions = [:left, :right]
+    else
+      directions = [:up, :down]
+    end
+    if on_top?
+      directions.delete(:up)
+    elsif on_bottom?
+      directions.delete(:down)
+    elsif on_left?
+      directions.delete(:left)
+    elsif on_right?
+      directions.delete(:right)
+    end
+    choice = rand(0..directions.size)
+    @direction = directions[choice]
+    move
+  end
+
+  def on_edge
+    on_left? || on_right? || on_top? || on_bottom?
+  end
+
+  def on_top?
+    @pos.y < 4
+  end
+
+  def on_bottom?
+    @pos.y + Constants::PLAYER_SIZE > 636
+  end
+
+  def on_left?
+    @pos.x < 4
+  end
+
+  def on_right?
+    @pos.x + Constants::PLAYER_SIZE > 636
   end
 end
 
